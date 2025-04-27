69f5d85afadedf764b85d5082670bb6fd557d5e1
Priyanka Mahajan <ws_htu1587@hoonartek03.net>
2025-04-27 15:09:22 +0530

    enabled the deployed version

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5174', 'https://book-app-frontend-tau.vercel.app'],
+    origin: ['http://localhost:5173', 'https://book-store-frontend-a58v.vercel.app/'],
     credentials: true
 }))
 
